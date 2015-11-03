9b3529b0e614f566dd63cefa99a78539eae36414
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-11-03 15:28:39 +0100

    start questions

---
+++ impression/Question.swift
@@ -0,0 +1,50 @@
+//
+//  Question.swift
+//  impression
+//
+//  Created by DAGUENET Valentin on 03/11/15.
+//  Copyright © 2015 Arthur Valentin. All rights reserved.
+//
+
+
+import UIKit
+
+
+class Question: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var answerTop: UILabel!
+    
+    @IBOutlet weak var answerBottom: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!{
+        didSet{
+            slider.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI_2))
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        self.label.text = "Lorem ipsum dolor";
+        self.answerTop.text = "I am the prop top";
+        self.answerBottom.text = "I am the prop bottom";
+    }
+    
+    @IBAction func onSlideEnd(sender: UISlider) {
+        var dest = 0.0;
+
+        if (sender.value > 0.5) {
+            dest = 1.0
+        }
+        
+        
+        UIView.beginAnimations(nil, context: nil);
+        UIView.setAnimationCurve(UIViewAnimationCurve.EaseOut);
+        UIView.setAnimationDuration(0.3);
+        self.slider.setValue(Float(dest), animated: true);
+        UIView.commitAnimations();
+        
+    }
+}
