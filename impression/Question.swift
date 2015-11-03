bf61c76de55b77934cd64fe49c5f4b1f8ddae67e
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-11-03 16:22:05 +0100

    set answer of the question

--- impression/Question.swift
+++ impression/Question.swift
@@ -11,13 +11,12 @@ import UIKit
 
 
 class Question: UIViewController {
+   
+    var selectedAnswer: String = ""
     
     @IBOutlet weak var label: UILabel!
-    
     @IBOutlet weak var answerTop: UILabel!
-    
     @IBOutlet weak var answerBottom: UILabel!
-    
     @IBOutlet weak var slider: UISlider!{
         didSet{
             slider.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI_2))
@@ -25,26 +24,28 @@ class Question: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad();
-        
-        self.label.text = "Lorem ipsum dolor";
-        self.answerTop.text = "I am the prop top";
-        self.answerBottom.text = "I am the prop bottom";
+        super.viewDidLoad()
+        self.label.text = "Lorem ipsum dolor"
+        self.answerTop.text = "I am the prop top"
+        self.answerBottom.text = "I am the prop bottom"
     }
     
     @IBAction func onSlideEnd(sender: UISlider) {
-        var dest = 0.0;
+        var dest = 0.0
 
         if (sender.value > 0.5) {
             dest = 1.0
+            self.selectedAnswer = self.answerTop.text!
+        } else {
+            self.selectedAnswer = self.answerBottom.text!
         }
+        print("answer \(self.selectedAnswer)")
         
-        
-        UIView.beginAnimations(nil, context: nil);
-        UIView.setAnimationCurve(UIViewAnimationCurve.EaseOut);
-        UIView.setAnimationDuration(0.3);
-        self.slider.setValue(Float(dest), animated: true);
-        UIView.commitAnimations();
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationCurve(UIViewAnimationCurve.EaseOut)
+        UIView.setAnimationDuration(0.3)
+        self.slider.setValue(Float(dest), animated: true)
+        UIView.commitAnimations()
         
     }
 }
