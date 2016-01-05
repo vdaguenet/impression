cc2609b1de18fbaaaf8544ba415661353b34660a
Valentin Daguenet <valentin.daguenet@gmail.com>
2016-01-05 16:35:06 +0100

    start store model

---
+++ impression/StoreModel.swift
@@ -0,0 +1,33 @@
+//
+//  StoreModel.swift
+//  impression
+//
+//  Created by DAGUENET Valentin on 05/01/16.
+//  Copyright © 2016 Arthur Valentin. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+struct StoreExpressions {
+    var id = SQLite.Expression<Int64>("id")
+    var name = SQLite.Expression<String>("name")
+    var adress = SQLite.Expression<String>("adress")
+    var city = SQLite.Expression<String>("city")
+    var hours = SQLite.Expression<String>("hours")
+    var lat = SQLite.Expression<Float>("lat")
+    var long = SQLite.Expression<Float>("long")
+}
+
+class StoreModel{
+    var table = Table("store")
+    let expressions = StoreExpressions()
+    
+    init() {}
+    
+    func findAll() -> [SQLite.Row] {
+        let rows = Array(try DB_CONNECTION.prepare(self.table))
+        
+        return rows
+    }
+}
