ade928c4fbd4ab04511be024d4a0c6b761c627cb
Valentin Daguenet <valentin.daguenet@gmail.com>
2016-01-07 10:21:30 +0100

    aniation intro

--- impression/IntroViewController.swift
+++ impression/IntroViewController.swift
@@ -9,11 +9,52 @@
 import UIKit
 
 class IntroViewController: UIViewController {
-
+    @IBOutlet weak var impression: UIImageView!
+    @IBOutlet weak var bySisley: UIImageView!
+    @IBOutlet weak var introText: UILabel!
+    @IBOutlet weak var circleButton: DualCircleButton!
     @IBOutlet weak var btnStart: UIButton!
         
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // prepare animations
+        self.impression.center.y += 50
+        self.impression.alpha = 0
+        self.bySisley.center.y += 50
+        self.bySisley.alpha = 0
+        
+        self.introText.center.y += 25
+        self.introText.alpha = 0
+        
+//        self.circleButton.center.y += 25
+        self.circleButton.alpha = 0
+//        self.btnStart.center.y += 25
+        self.btnStart.alpha = 0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // animate
+        UIView.animateWithDuration(1.0, delay: 0.0, options: [ .CurveEaseOut ], animations: {
+            self.impression.center.y -= 50
+            self.impression.alpha = 1
+            }, completion: nil)
+        UIView.animateWithDuration(1.0, delay: 0.15, options: [ .CurveEaseOut ], animations: {
+            self.bySisley.center.y -= 50
+            self.bySisley.alpha = 1
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1.0, delay: 0.8, options: [ .CurveEaseOut ], animations: {
+            self.introText.center.y -= 25
+            self.introText.alpha = 1
+            }, completion: nil)
+        
+        UIView.animateWithDuration(2.0, delay: 1.4, options: [ .CurveEaseOut ], animations: {
+            self.btnStart.alpha = 1
+            self.circleButton.alpha = 1
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
